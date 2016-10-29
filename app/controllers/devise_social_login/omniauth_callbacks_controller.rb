58876f2ee6b06c68821fddda67bc3505f04207c8
blueplanet <erguolinge@gmail.com>
2016-10-29 18:43:28 +0900

    Add facebook method

--- app/controllers/devise_social_login/omniauth_callbacks_controller.rb
+++ app/controllers/devise_social_login/omniauth_callbacks_controller.rb
@@ -1,5 +1,24 @@
 module DeviseSocialLogin
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     include Devise::Controllers::Rememberable
+
+    def facebook
+      auth = request.env['omniauth.auth']
+
+      @user = User.from_auth auth
+      if @user.persisted?
+        sign_in_and_redirect @user, event: :authentication
+        set_flash_message :notice, :success, kind: 'Facebook' if is_navigational_format?
+      else
+        session['devise.facebook_data'] = auth
+        redirect_to new_user_registration_url
+      end
+    end
+
+    private
+
+      def failure
+        redirect_to root_path
+      end
   end
 end
