3360011d3ba701dffeac339d71aa4ff81b8f54d1
blueplanet <erguolinge@gmail.com>
2016-10-31 08:54:38 +0900

    Add UserFromAuth concern

---
+++ app/models/devise_social_login/concerns/user_from_auth.rb
@@ -0,0 +1,21 @@
+module UserFromAuth
+  extend ActiveSupport::Concern
+  included do
+    has_many :social_profiles
+  end
+
+  module ClassMethods
+    def from_auth(auth)
+      find_or_initialize_by(email: auth.info.email).tap do |user|
+        if user.new_record?
+          user.password = Devise.friendly_token[0, 20]
+          user.confirmed_at = Time.current
+        end
+
+        user.social_profiles.find_or_initialize_by(provider: :facebook, uid: auth.uid)
+
+        user.save!
+      end
+    end
+  end
+end
