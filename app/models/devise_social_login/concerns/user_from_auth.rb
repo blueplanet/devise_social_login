1cc21ecd54c1efa58b1bec9c2b64cbf720cdfd6e
blueplanet <erguolinge@gmail.com>
2016-10-31 08:59:03 +0900

    Add module

--- app/models/devise_social_login/concerns/user_from_auth.rb
+++ app/models/devise_social_login/concerns/user_from_auth.rb
@@ -1,22 +1,24 @@
-module UserFromAuth
-  extend ActiveSupport::Concern
-  included do
-    has_many :social_profiles
+module DeviseSocialLogin
+  module UserFromAuth
+    extend ActiveSupport::Concern
+    included do
+      has_many :social_profiles
 
-    devise :omniauthable, omniauth_providers: [:facebook]
-  end
+      devise :omniauthable, omniauth_providers: [:facebook]
+    end
 
-  module ClassMethods
-    def from_auth(auth)
-      find_or_initialize_by(email: auth.info.email).tap do |user|
-        if user.new_record?
-          user.password = Devise.friendly_token[0, 20]
-          user.confirmed_at = Time.current
+    module ClassMethods
+      def from_auth(auth)
+        find_or_initialize_by(email: auth.info.email).tap do |user|
+          if user.new_record?
+            user.password = Devise.friendly_token[0, 20]
+            user.confirmed_at = Time.current
+          end
+
+          user.social_profiles.find_or_initialize_by(provider: :facebook, uid: auth.uid)
+
+          user.save!
         end
-
-        user.social_profiles.find_or_initialize_by(provider: :facebook, uid: auth.uid)
-
-        user.save!
       end
     end
   end
